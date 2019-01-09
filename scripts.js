dcbe56360cedc268fda68c90479e6f2c54b7c64a
peter patel <ngash.waeru@gmail.com>
2019-01-09 16:47:45 +0300

    my projo

--- scripts.js
+++ scripts.js
@@ -1,8 +1,47 @@
-var modal = document.getElementById('id01');
+//bid click interact
+var modal = document.getElementById("myModal");
+//GET Button that opens modal
+var btn = document.getElementById('addTo');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function(){
+    modal.style.display = "block";
 }
+
+//user click on the (X)
+span.onclick = function(){
+    modal.style.display = "none";
+}
+
+ //user clicks on the outside
+ window.onclick = function(){
+     if(event.target == modal){
+        modal.style.display = "none";
+     }
+     
+ }
+
+ //form
+ var form = document.getElementById("myForm");
+
+ var linkLog = document.getElementById("loginTo");
+ // Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+ linkLog.onclick = function(){
+     form.style.display = "block";
+ }
+ span.onclick = function(){
+     form.style.display = "none"
+ }
+ window.onclick = function(){
+    if(event.target == form){
+       form.style.display = "none";
+    }
+    
+}
+
