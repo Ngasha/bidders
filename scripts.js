f03986f269b4ba2f4b77826e5bd5373bc15abbdc
peter patel <ngash.waeru@gmail.com>
2019-01-16 14:52:16 +0300

    my final project

--- scripts.js
+++ scripts.js
@@ -25,7 +25,7 @@ span.onclick = function(){
      
  }
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 17, 2019 12:58:25").getTime();
+var countDownDate = new Date("Jan 24, 2019 12:58:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -36,15 +36,32 @@ var x = setInterval(function() {
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
     
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var items = document.querySelectorAll('.item');
+  items.forEach(function(item) {
+      var itemDeadline = item.querySelector('.deadline');
+      if(itemDeadline) {
+        writeDeadline(itemDeadline);
+      }
+  })
+
+  function calculateDeadline() {
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var interval = Math.random();
+    var dayInterval = interval * days;
+    var newDays = Math.ceil(dayInterval);
+    var hours = Math.ceil(interval * 24);
+    var minutes = Math.ceil(interval * 60);
+    var seconds = Math.ceil(interval * 60);
+    return [newDays, hours, minutes, seconds]
+  }
     
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  function writeDeadline(element) {
+    // Output the result in an element with id="demo"
+    var deadline = calculateDeadline();
+    element.innerHTML = deadline[0] + "d " + deadline[1] + "h "
+    + deadline[2] + "m " + deadline[3] + "s ";
+  }
     
   // If the count down is over, write some text 
   if (distance < 0) {
@@ -52,4 +69,6 @@ var x = setInterval(function() {
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+
 
